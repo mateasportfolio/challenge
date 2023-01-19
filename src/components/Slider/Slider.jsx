6ce25f1723f5823ac6da02b80e9c63b465108ad4
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2023-01-19 22:08:59 +0100

    add react slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import "react-slideshow-image/dist/styles.css";
+import { Slide } from "react-slideshow-image";
 
 export const Slider = () => {
-  return <div>Slider</div>;
+  const images = [
+    "https://lp2.sme-static.com/eisbrecher/dist/img/EB_LMM_Cover_3000x3000.jpg",
+    "https://lp2.sme-static.com/eisbrecher/dist/img/EB_Single_FAKK_3000x3000.jpg",
+  ];
+  return (
+    <Slide>
+      <div className="each-slide-effect">
+        <div style={{ backgroundImage: `url(${images[0]})` }}>
+          <span>Slide 1</span>
+        </div>
+      </div>
+      <div className="each-slide-effect">
+        <div style={{ backgroundImage: `url(${images[1]})` }}>
+          <span>Slide 2</span>
+        </div>
+      </div>
+    </Slide>
+  );
 };
