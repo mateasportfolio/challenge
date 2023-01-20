5e64ceff495b3f86ef3387d758314165366257fb
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2023-01-20 13:58:46 +0100

    install konami-code and add Konami.jsx

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import { ImageSlider } from "./components/Slider/ImageSlider";
 import { SliderData } from "./components/Slider/SliderData";
 import Subscribe from "./components/Subscribe/Subscribe";
 import TextC from "./components/TextC/TextC";
+import Konami from "./components/Konami/Konami.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <ImageSlider slides={SliderData} />
+      <Konami />
       <Subscribe />
       <TextC />
       <Footer />
