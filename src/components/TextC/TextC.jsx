b76e62e2ccf7a434e3cfa89d14383481d8fca828
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2023-01-20 11:12:45 +0100

    add border and column in TextC.jsx

--- src/components/TextC/TextC.jsx
+++ src/components/TextC/TextC.jsx
@@ -1,28 +1,54 @@
 import React from "react";
-import { Box, Flex, Text, Square } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 const TextC = () => {
   return (
-    <Flex color="white" flexDirection={"row"}>
-      <Box w="100px" bg="green.500">
-        <Text>Was ist hier los?</Text>
-      </Box>
-      <Square w="100px" bg="blue.500">
-        <Text>Besser</Text>
-      </Square>
-      <Box w="100px" bg="red.500">
-        <Text> Sturmfahrt</Text>
-      </Box>
-      <Box w="100px" bg="green.500">
-        <Text> In einem Boot</Text>
-      </Box>
-      <Box w="100px" bg="blue.500">
-        <Text> Automat</Text>
-      </Box>
-      <Box w="100px" bg="red.500">
-        <Text> Eisbär (Grauzone-Cover)</Text>
-      </Box>
-    </Flex>
+    <>
+      <Flex
+        color="white"
+        flexDirection={"column"}
+        border={"2px solid red"}
+        display="flex"
+        alignContent="center"
+        textAlign="center"
+        fontSize="xl"
+        justifyContent="center"
+        flexWrap="nowrap"
+        alignItems="center"
+      >
+        <Box
+          w="200px"
+          bg="green.500"
+          flexDirection="column"
+          alignContent="center"
+          textAlign="center"
+          fontSize="xl"
+          justifyContent="center"
+          flexWrap="nowrap"
+          display="flex"
+          alignItems="center"
+        >
+          <Text display={"flex"} flexDirection={"column"}>
+            Was ist hier los?
+          </Text>
+        </Box>
+        <Box w="200px" bg="blue.500" flexDirection="column">
+          <Text>Besser</Text>
+        </Box>
+        <Box w="200px" bg="red.500">
+          <Text> Sturmfahrt</Text>
+        </Box>
+        <Box w="200px" bg="green.500">
+          <Text> In einem Boot</Text>
+        </Box>
+        <Box w="200px" bg="blue.500">
+          <Text> Automat</Text>
+        </Box>
+        <Box w="200px" bg="red.500">
+          <Text> Eisbär (Grauzone-Cover)</Text>
+        </Box>
+      </Flex>
+    </>
   );
 };
 
