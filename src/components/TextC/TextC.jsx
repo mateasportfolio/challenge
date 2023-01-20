09558551333cd4d0ee502edd6b875448b1b04380
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2023-01-20 11:58:38 +0100

    add border in TextC.jsx for better understanding

--- src/components/TextC/TextC.jsx
+++ src/components/TextC/TextC.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text, Image } from "@chakra-ui/react";
+import cover from "../../assets/cover.jpeg";
+import melodie from "../../assets/melodie.png";
 
 const TextC = () => {
   return (
     <>
-      <Flex
+      {/* <Flex
         color="white"
         flexDirection={"column"}
         border={"2px solid red"}
@@ -16,7 +18,7 @@ const TextC = () => {
         flexWrap="nowrap"
         alignItems="center"
       >
-        <Box
+        <Flex
           w="200px"
           color="green.500"
           flexDirection="column"
@@ -27,32 +29,119 @@ const TextC = () => {
           flexWrap="nowrap"
           display="flex"
           alignItems="center"
-          border="1px solid white"
+          border="1px solid red"
         >
           <Text display={"flex"} flexDirection={"column"}>
             Was ist hier los?
           </Text>
-        </Box>
+        </Flex>
         <Box
           w="200px"
           color="blue.500"
-          border="1px solid white"
+          border="4px solid teal"
           flexDirection="column"
         >
           <Text>Besser</Text>
         </Box>
-        <Box w="200px" color="red.500" border="1px solid white">
+        <Box w="200px" color="red.500" border="1px solid purple">
           <Text> Sturmfahrt</Text>
         </Box>
-        <Box w="200px" color="green.500" border="1px solid white">
+        <Box w="200px" color="green.500" border="1px solid purple">
           <Text> In einem Boot</Text>
         </Box>
-        <Box w="200px" color="blue.500" border="1px solid white">
+        <Box w="200px" color="blue.500" border="1px solid purple">
           <Text> Automat</Text>
         </Box>
-        <Box w="200px" color="red.500" border="1px solid white">
+        <Box w="200px" color="red.500" border="1px solid purple">
           <Text> Eisbär (Grauzone-Cover)</Text>
         </Box>
+      </Flex> */}
+
+      <Flex
+        wrap="wrap"
+        gap="4px"
+        alignItems="center"
+        justifyContent="center"
+        border={"1px solid red"}
+      >
+        <Flex border="1px solid pink" flexDirection="column" cursor="pointer">
+          <Box
+            color="green.500"
+            flexDirection="column"
+            alignContent="center"
+            textAlign="center"
+            fontSize="xl"
+            justifyContent="center"
+            flexWrap="nowrap"
+            display="flex"
+            alignItems="center"
+            border="1px solid yellow"
+          >
+            <Text display={"flex"} flexDirection={"column"}>
+              Was ist hier los?
+            </Text>
+
+            <Text display={"flex"} flexDirection={"column"}>
+              Besser
+            </Text>
+            <Text display={"flex"} flexDirection={"column"}>
+              {" "}
+              Sturmfahrt
+            </Text>
+            <Text display={"flex"} flexDirection={"column"}>
+              {" "}
+              In einem Boot
+            </Text>
+            <Text display={"flex"} flexDirection={"column"}>
+              {" "}
+              Automat
+            </Text>
+            <Text display={"flex"} flexDirection={"column"}>
+              {" "}
+              Eisbär (Grauzone-Cover)
+            </Text>
+            <Image
+              // src={cover}
+              height="320px"
+              width="320px"
+              objectFit="cover"
+              borderRadius="10px"
+            />
+          </Box>
+        </Flex>
+        <Flex border="1px solid pink" flexDirection="column" cursor="pointer">
+          <Text
+            marginBottom="10px"
+            alignItems="center"
+            justifyContent="center"
+            textAlign="center"
+          >
+            Image
+          </Text>
+          <Image
+            src={melodie}
+            height="320px"
+            width="320px"
+            objectFit="cover"
+            borderRadius="10px"
+          />
+        </Flex>
+        <Flex border="1px solid red" flexDirection="column" cursor="pointer">
+          <Text
+            marginBottom="10px"
+            alignItems="center"
+            justifyContent="center"
+            textAlign="center"
+          >
+            Video
+          </Text>
+          <Image
+            height="320px"
+            width="320px"
+            objectFit="cover"
+            borderRadius="10px"
+          />
+        </Flex>
       </Flex>
     </>
   );
