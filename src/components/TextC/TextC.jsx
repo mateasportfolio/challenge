95ff0bfa79a6b8a2c7ea8bb698087a1b1e0a8135
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2023-01-20 13:19:40 +0100

    aligned text in flexbox

--- src/components/TextC/TextC.jsx
+++ src/components/TextC/TextC.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Flex, Text, Image } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 import melodie from "../../assets/melodie.png";
 
 const TextC = () => {
@@ -8,19 +8,38 @@ const TextC = () => {
       <Flex
         border={"1px solid red"}
         p={200}
+        m={150}
         gap="40px"
         alignItems="center"
         justifyContent="center"
       >
         <Box
+          overflow="hidden"
           flexDirection="row"
           gap="40px"
           alignItems="center"
           justifyContent="center"
           border="2px solid pink"
+          //   height="350px"
+          //   width="800px"
+          w="200vh"
+          h="50vh"
+          alignContent="center"
+          textAlign="center"
+          fontSize="xl"
         >
           {" "}
-          Text
+          <p> Was ist hier los?</p>
+          <span></span>
+          <p> Besser</p>
+          <span></span>
+          <p> Sturmfahrt</p>
+          <span></span>
+          <p> In einem Boot</p>
+          <span></span>
+          <p> Automat</p>
+          <span></span>
+          <p> Eisbär (Grauzone-Cover)</p>
         </Box>
         <Box
           flexDirection="row"
@@ -28,8 +47,17 @@ const TextC = () => {
           alignItems="center"
           justifyContent="center"
           border="2px solid pink"
+          w="200vh"
+          h="50vh"
         >
           {" "}
+          <Image
+            src={melodie}
+            objectFit="cover"
+            borderRadius="10px"
+            w="100%"
+            h="50vh"
+          />
           Image
         </Box>
         <Box
@@ -38,8 +66,17 @@ const TextC = () => {
           alignItems="center"
           justifyContent="center"
           border="2px solid pink"
+          w="200vh"
+          h="50vh"
         >
           {" "}
+          <Image
+            // src={melodie}
+            height="320px"
+            width="320px"
+            objectFit="cover"
+            borderRadius="10px"
+          />
           Video
         </Box>
       </Flex>
